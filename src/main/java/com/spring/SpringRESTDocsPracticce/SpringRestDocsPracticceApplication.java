ffba1e193bb9619b57a676008ce4aa80734be144
lhwn <cjsrnr1674@hanmail.net>
2021-05-26 09:03:16 +0900

    Set an environment, add a methods

---
+++ src/main/java/com/spring/SpringRESTDocsPracticce/SpringRestDocsPracticceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SpringRESTDocsPracticce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsPracticceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDocsPracticceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/SpringRESTDocsPracticce/SpringRestDocsPracticceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringRESTDocsPracticce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsPracticceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
